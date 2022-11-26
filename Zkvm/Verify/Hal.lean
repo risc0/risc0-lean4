e7b77155053b5c3d4ee1d65f80897f9498349ba6
Tim Carstens <tim@risczero.com>
2022-11-25 17:37:43 -0800

    Subs for Hal functions

--- Zkvm/Verify/Hal.lean
+++ Zkvm/Verify/Hal.lean
@@ -2,6 +2,29 @@
 Copyright (c) 2022 RISC Zero. All rights reserved.
 -/
 
+import R0sy.Algebra.Field.BabyBear
+import Zkvm.Taps
+
 namespace Zkvm.Verify.Hal
+
+open R0sy.Algebra.Field
+open Taps
+
+def computePolynomial (u: Subarray BabyBear.ExtElem) (poly_mix: BabyBear.ExtElem) (out mix: Subarray BabyBear.Elem): BabyBear.ExtElem := sorry
+
+def foldEval [Monad M] [MonadStateOf (Array BabyBear.ExtElem) M] (x: BabyBear.ExtElem): BabyBear.ExtElem := sorry
+
+def polyEval (coeffs: Subarray BabyBear.ExtElem) (x: BabyBear.ExtElem): BabyBear.ExtElem := sorry
+
+def friEvalTaps
+  (taps: TapSet)
+  (mix: BabyBear.ExtElem)
+  (combo_u: Subarray BabyBear.ExtElem)
+  (check_row: Subarray BabyBear.Elem)
+  (back_one: BabyBear.Elem)
+  (x: BabyBear.Elem)
+  (z: BabyBear.ExtElem)
+  (rows: Array (Subarray BabyBear.Elem))
+  : BabyBear.ExtElem := sorry
 
 end Zkvm.Verify.Hal
