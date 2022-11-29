e2baad62debd9d05f495275962e00740821bd3ff
Bolton Bailey <bolton.bailey@gmail.com>
2022-11-29 11:24:51 -0600

    Style for monadic programming

--- Zkvm/Verify/Merkle.lean
+++ Zkvm/Verify/Merkle.lean
@@ -40,32 +40,32 @@ def HashRawPodSlice (val : Array Elem) : R0sy.Hash.Sha2.Sha256.Digest := sorry -
 def HashPair (a b : Sha256.Digest) : Sha256.Digest := sorry -- TODO does this already exist?
 
 def MerkleTreeVerifier.verify [Monad M] [MonadReadIop M] [MonadExceptOf VerificationError M] [MonadStateOf Nat M] [R0sy.Algebra.Field Elem] 
-  (self: MerkleTreeVerifier) (idx_: Nat): M (Array Elem)
-  := do let mut idx := idx_
-        if idx >= 2 * self.params.row_size 
-          then throw (VerificationError.MerkleQueryOutOfRange idx self.params.row_size) 
-          else
-        let out <- MonadReadIop.readFields Elem self.params.col_size
-        let mut cur := HashRawPodSlice out
-        idx := idx + self.params.row_size
-        while idx >= 2 * self.params.row_size do
-          let low_bit := idx % 2
-          let otherArray <- MonadReadIop.readPodSlice 1
-          let other := otherArray.get! 0
-          idx := idx / 2
-          if low_bit == 1 
-          then 
-            cur := HashPair other cur
-          else 
-            cur := HashPair cur other
-        let present_hash := 
-          if idx >= self.params.top_size 
-            then self.top.get! (self.params.idx_to_top idx) 
-            else self.top.get! (self.params.idx_to_rest idx)
-        if present_hash == cur 
-        then
-          return out
-        else 
-          throw VerificationError.InvalidProof
+  (self: MerkleTreeVerifier) (idx_: Nat): M (Array Elem) := do 
+  let mut idx := idx_
+  if idx >= 2 * self.params.row_size 
+    then throw (VerificationError.MerkleQueryOutOfRange idx self.params.row_size) 
+    else
+  let out <- MonadReadIop.readFields Elem self.params.col_size
+  let mut cur := HashRawPodSlice out
+  idx := idx + self.params.row_size
+  while idx >= 2 * self.params.row_size do
+    let low_bit := idx % 2
+    let otherArray <- MonadReadIop.readPodSlice 1
+    let other := otherArray.get! 0
+    idx := idx / 2
+    if low_bit == 1 
+    then 
+      cur := HashPair other cur
+    else 
+      cur := HashPair cur other
+  let present_hash := 
+    if idx >= self.params.top_size 
+      then self.top.get! (self.params.idx_to_top idx) 
+      else self.top.get! (self.params.idx_to_rest idx)
+  if present_hash == cur 
+  then
+    return out
+  else 
+    throw VerificationError.InvalidProof
 
 end Zkvm.Verify.Merkle
