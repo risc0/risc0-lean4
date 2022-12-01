39bf74fc453081a504b70561dd1e942d6e98a245
Bolton Bailey <bolton.bailey@gmail.com>
2022-12-01 14:12:57 -0600

    comment explaining the degree

--- R0sy/Algebra/Field.lean
+++ R0sy/Algebra/Field.lean
@@ -284,7 +284,7 @@ instance [Field F] [Ring R] [PolyRing F R] [DivRemRing R] [GcdRing R] {q: Irredu
 instance [Field F] [Ring R] [PolyRing F R] [DivRemRing R] [GcdRing R] {q: Irreducible F R} : Algebra F (Elem q) where
   hMul := sorry
   ofBase := Elem.ofBase _
-  EXT_SIZE := sorry
+  EXT_SIZE := sorry -- The degree of the field extension
 
 end Ext
 
