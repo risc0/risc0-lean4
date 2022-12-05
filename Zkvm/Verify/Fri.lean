636156372ae38ee8a53971460336181f85e41c9e
Bolton Bailey <bolton.bailey@gmail.com>
2022-12-04 22:32:50 -0600

    implement fri_verify, minus inner

--- Zkvm/Verify/Fri.lean
+++ Zkvm/Verify/Fri.lean
@@ -68,5 +68,61 @@ def VerifyRoundInfo.verify_query [Monad M] [MonadReadIop M] [MonadExceptOf Verif
   goal.set new_goal
   pure ()
 
+def fri_verify [Monad M] [MonadReadIop M] [MonadExceptOf VerificationError M] [rng : MonadRng M]
+  [RootsOfUnity Elem]
+  (pos : MonadStateOf Nat M) (goal : MonadStateOf ExtElem M)
+  [R0sy.Algebra.Field Elem] [R0sy.Algebra.Field ExtElem] [e: R0sy.Algebra.Algebra Elem ExtElem]
+  (degree : Nat) (inner : Monad M -> Nat -> ExtElem) : M Unit := do
+    let mut degree_ := degree
+    let orig_domain := INV_RATE * degree
+    let mut domain := orig_domain
+    -- Prep the folding verfiers
+    let rounds_capacity := ((Nat.log2_ceil ((degree + FRI_FOLD - 1) / FRI_FOLD)) + FRI_FOLD_PO2 - 1) / FRI_FOLD_PO2 -- this is just for performance in the rust
+    let mut rounds : Array (VerifyRoundInfo ExtElem) := Array.mkEmpty rounds_capacity
+    while degree > FRI_MIN_DEGREE do
+      let round <- @VerifyRoundInfo.new _ ExtElem Elem _ _ _ _ _ domain
+      rounds := rounds.push round
+      domain := domain / FRI_FOLD
+      degree_ := degree_ / FRI_FOLD
+
+    -- // We want to minimize reallocation in verify, so make sure we
+    -- // didn't have to reallocate.
+    -- assert!(
+    --     rounds.len() < rounds_capacity,
+    --     "Did not allocate enough rounds; needed {} for degree {} but only allocated {}",
+    --     rounds.len(),
+    --     degree,
+    --     rounds_capacity
+    -- );
+    -- // Grab the final coeffs + commit
+    let final_coeffs : Array Elem <- MonadReadIop.readFields Elem (e.EXT_SIZE * degree)
+    let final_digest := Hash.hash_pod (final_coeffs)
+    MonadReadIop.commit final_digest
+
+    -- // Get the generator for the final polynomial evaluations
+    let gen : Elem := RootsOfUnity.ROU_FWD[Nat.log2_ceil (domain)]!
+    -- // Do queries
+    let mut poly_buf: Array ExtElem := Array.mkEmpty degree_
+    for _ in [0:QUERIES] do
+      let rng: UInt32 <- @MonadRng.nextUInt32 _ rng
+      pos.set ((rng.toNat) % orig_domain)
+      -- // Do the 'inner' verification for this index
+      goal.set sorry --inner(iop, pos)?
+      -- // Verify the per-round proofs
+      for round in rounds do
+        @VerifyRoundInfo.verify_query _ Elem ExtElem _ _ _ _ pos goal _ _ _ round
+      
+      -- // Do final verification
+      let x : Elem := gen ^ (<- pos.get)
+
+      let collate_final_coeffs : Array (Array Elem) := sorry -- collect field elements into groups of size EXT_SIZE
+      poly_buf := collate_final_coeffs.map from_subelems
+
+      let fx : ExtElem := poly_eval poly_buf (e.ofBase x)
+      if fx != (<- goal.get)
+        then
+          throw VerificationError.InvalidProof -- XXX surprised this is valid syntax (without the "else"), I assume it does what it looks like it does?
+    return ()
+
 
 end Zkvm.Verify.Fri
