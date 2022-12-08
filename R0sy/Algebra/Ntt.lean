37ffcefa480b2c893ae335b3eb57fa37b67e8788
Bolton Bailey <bolton.bailey@gmail.com>
2022-12-08 13:31:51 -0800

    interpolate ntt

--- R0sy/Algebra/Ntt.lean
+++ R0sy/Algebra/Ntt.lean
@@ -3,12 +3,15 @@ Copyright (c) 2022 RISC Zero. All rights reserved.
 -/
 
 import R0sy.Algebra
+import R0sy.Lean.Nat
 -- import Zkvm.ArithVM.Circuit
 -- import Zkvm.Constants
 -- import Zkvm.Verify.Classes
 -- import Zkvm.Verify.Merkle
 
 namespace R0sy.Algebra.Ntt
+
+open R0sy.Lean.Nat
 
 /-- Reverses the bits of n, treating n as a b-bit binary number -/
 partial def reverse_bits (n b : Nat) : Nat :=
@@ -21,6 +24,8 @@ instance [Add T] : Add (Array T) where add x y := (Array.zip x y).map (λ (a,b) 
 instance [Sub T] : Sub (Array T) where sub x y := (Array.zip x y).map (λ (a,b) => a - b)
 
 instance [Mul T] : Mul (Array T) where mul x y := (Array.zip x y).map (λ (a,b) => a * b)
+
+instance [Mul T] : HMul (Array T) T (Array T) where hMul x y := x.map (λ a => a * y)
 
 instance [HPow S T S] : HPow S (Array T) (Array S) where hPow x n := n.map (λ k => x ^ k)
 
@@ -46,5 +51,9 @@ partial def rev_butterfly [Field ExtElem] [RootsOfUnity ExtElem] (n : Nat) (arr 
       let first_half : Array ExtElem := a + b;
       let second_half : Array ExtElem := (a - b) * ((RootsOfUnity.ROU_REV[n]! : ExtElem) ^ ((List.range n).toArray) : Array ExtElem);
       first_half ++ second_half
+
+partial def interpolate_ntt [Field ExtElem] [RootsOfUnity ExtElem] (io : Array ExtElem) : Array ExtElem :=
+  let n := Nat.log2_ceil io.size;
+  (rev_butterfly n io) * (Field.inv (Field.fromUInt64 io.size.toUInt64 : ExtElem))
 
 end R0sy.Algebra.Ntt
