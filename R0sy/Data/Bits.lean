f6f88aa1b30362a1c535d12075c42b2c28149368
Timothy Carstens <intoverflow@gmail.com>
2022-12-10 22:09:12 +0000

    Rename max to least_upper_bound

--- R0sy/Data/Bits.lean
+++ R0sy/Data/Bits.lean
@@ -4,19 +4,19 @@ Copyright (c) 2022 RISC Zero. All rights reserved.
 
 namespace R0sy.Data.Bits
 
-def Bits.max (hi lo: UInt32): Nat
-  := 2 ^ (1 + hi.toNat - lo.toNat)
+def Bits.least_upper_bound (hi lo: UInt32): Nat
+  := 1 <<< (1 + hi.toNat - lo.toNat)
 
 def Bits.mask (hi lo: UInt32): UInt32
-  := (Bits.max hi lo).toUInt32 <<< lo
+  := (Bits.least_upper_bound hi lo - 1).toUInt32 <<< lo
 
 structure Bits (hi lo: UInt32) where
-  val: UInt32 -- Fin (Bits.max hi lo)
+  val: UInt32 -- Fin (Bits.least_upper_bound hi lo)
   deriving BEq
 
 def Bits.ofUInt32 (x: UInt32): Bits hi lo
   := {
-    val := (x >>> lo) &&& (Bits.max hi lo - 1).toUInt32
+    val := (x >>> lo) &&& (Bits.least_upper_bound hi lo - 1).toUInt32
   }
 
 def Bits.toUInt32 (x: Bits hi lo): UInt32
