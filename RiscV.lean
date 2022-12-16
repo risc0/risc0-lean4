7b27768fffb9c2f2a8661da89b1f48330e298d42
Timothy Carstens <intoverflow@gmail.com>
2022-12-16 04:22:33 +0000

    Start of Config and XlenInt

--- RiscV.lean
+++ RiscV.lean
@@ -2,6 +2,7 @@
 Copyright (c) 2022 RISC Zero. All rights reserved.
 -/
 
+import RiscV.Config
 import RiscV.Instr.RV32I
 import RiscV.Instr.RV32M
 import RiscV.Instr.Types
@@ -11,3 +12,4 @@ import RiscV.Mach.Int
 import RiscV.Mach.Mem
 import RiscV.Mach.Reg
 import RiscV.Monad
+import RiscV.XlenInt
