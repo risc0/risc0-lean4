7c77e2f117e550b83826bd4c9bbaa848301ab0d7
Timothy Carstens <intoverflow@gmail.com>
2022-12-25 16:02:14 -0800

    Add mathlib4

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,9 @@ package «risc0-lean4» {
   -- add package configuration options here
 }
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 @[default_target]
 lean_lib Elf {
   -- add library configuration options here
